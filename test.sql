297d9a2948062ba6883b84b65964df7b3fb98389
yuchen <cy970802@hotmail.com>
2020-09-10 08:45:28 +0200

    update accountdb

--- test.sql
+++ test.sql
@@ -25,15 +25,17 @@ CREATE TABLE `accountdb` (
   `username` varchar(20) DEFAULT NULL,
   `pwd` varchar(50) DEFAULT NULL,
   `balance` double DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `cellphone` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
 
 /*Data for the table `accountdb` */
 
-insert  into `accountdb`(`id`,`username`,`pwd`,`balance`) values 
-(1,'Alice','00000',100),
-(2,'Bob','11111',200),
-(3,'Charlie','22222',300);
+insert  into `accountdb`(`id`,`username`,`pwd`,`balance`,`email`,`cellphone`) values 
+(1,'Alice','00000',50,'00000@000.com','00000000'),
+(2,'Bob','11111',200,'11111@111.com','11111111'),
+(3,'Charlie','22222',300,'22222@222.com','22222222');
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
 /*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
